219acdf9afd109d7e35e91df8a7c9c356223b64e
Neehal P <neehalp9988@gmail.com>
2026-02-18 07:30:13 +0000

    Fix main.py structure

--- main.py
+++ main.py
@@ -1,32 +1,40 @@
-@app.get("/")
-def home():
-    return {"message": "CI/CD pipeline verified 🚀🔥"}
 from fastapi import FastAPI
 from sqlalchemy import create_engine, Column, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+# Create FastAPI app
+app = FastAPI()
+
+# Database connection
 DATABASE_URL = "postgresql://postgres:password@postgres:5432/postgres"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
 
+
+# Student table model
 class Student(Base):
     __tablename__ = "students"
+
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
     marks = Column(Float)
     attendance = Column(Float)
 
+
+# Create tables in database
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
 
+# Home route
 @app.get("/")
 def home():
     return {"message": "CI/CD pipeline verified 🚀🔥"}
 
+
+# Add student
 @app.post("/students")
 def add_student(name: str, marks: float, attendance: float):
     db = SessionLocal()
@@ -37,6 +45,8 @@ def add_student(name: str, marks: float, attendance: float):
     db.close()
     return student
 
+
+# Get all students
 @app.get("/students")
 def get_students():
     db = SessionLocal()
